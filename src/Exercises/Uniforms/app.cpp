2778f85b22b77cc4a64655226a908f112e863877
izabela-lukaszek <73258446+izabela-lukaszek@users.noreply.github.com>
2020-11-04 08:52:35 +0100

    Add files via upload

---
+++ src/Exercises/Uniforms/app.cpp
@@ -0,0 +1,171 @@
+//
+// Created by pbialas on 25.09.2020.
+//
+
+#include "app.h"
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+
+#include "Application/utils.h"
+
+void SimpleShapeApplication::init() {
+
+
+    auto program = xe::create_program(std::string(PROJECT_DIR) + "/shaders/base_vs.glsl",
+                                      std::string(PROJECT_DIR) + "/shaders/base_fs.glsl");
+
+
+    if (!program) {
+        std::cerr << "Cannot create program from " << std::string(PROJECT_DIR) + "/shaders/base_vs.glsl" << " and ";
+        std::cerr << std::string(PROJECT_DIR) + "/shaders/base_fs.glsl" << " shader files" << std::endl;
+    }
+
+    std::vector<GLushort> indices = {
+            0,1,2,3,4,5,6,7,8,
+    };
+    GLuint idx_buffer_handle;
+    glGenBuffers(1,&idx_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(),GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    auto u_modifiers_index = glGetUniformBlockIndex(program, "Modifiers");
+    if (u_modifiers_index == GL_INVALID_INDEX){
+        std::cout << "Cannot find Modifiers uniform block in program" << std::endl;
+    }
+    else {
+        glUniformBlockBinding(program, u_modifiers_index, 0);
+    }
+
+    GLuint ubo_handle(0u);
+    glGenBuffers(1,&ubo_handle);
+
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo_handle);
+    float light_intensity=0.5;
+    float light_color[3] = {1.0,1.0,1.0};
+    
+    glBufferData(GL_UNIFORM_BUFFER, 8 * sizeof(float), nullptr, GL_STATIC_DRAW);
+
+    glBufferSubData(GL_UNIFORM_BUFFER,0,sizeof(float),&light_intensity);
+    glBufferSubData(GL_UNIFORM_BUFFER,4 * sizeof(float),3 * sizeof(float),light_color);
+
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    glBindBufferBase(GL_UNIFORM_BUFFER, 0, ubo_handle);
+
+//Wartości wychodzące poza przedział (-1,1) spowodują że trojkąt będzie przy danym wierzchołku wychodził poza obszar okna
+//Zmiana wartości z nic nie zmienia w pokazywanym na ekranie trójkącie jednak sprawia ze trojkąt staje sie bryłą przestrzenną jednak żeby to zobaczyć w oknie należaloby wykonać animcje obrotu
+    std::vector<GLfloat> vertices = {
+            -0.5f, -0.5f, 0.0f,
+            0.0f,0.0f,1.0f,1.0f,
+            -0.5f, 0.0f, 0.0f,
+            0.0f,0.0f,1.0f,1.0f,
+            0.0f, 0.0f, 0.0f,
+            0.0f,0.0f,1.0f,1.0f,
+
+    };
+
+
+    std::vector<GLfloat> vertices2 = {
+            0.0f, 0.0f, 0.0f,
+            0.0f,0.0f,1.0f,1.0f,
+            0.0f, -0.5f, 0.0f,
+            0.0f,0.0f,1.0f,1.0f,
+            -0.5f, -0.5f, 0.0f,
+            0.0f,0.0f,1.0f,1.0f,
+
+    };
+
+
+    std::vector<GLfloat> vertices3 = {
+            -0.7f, 0.0f, 0.0f,
+            1.0f,0.0f,0.0f,1.0f,
+            0.2f, 0.0f, 0.0f,
+            1.0f,0.0f,0.0f,1.0f,
+            -0.25f, 0.6f, 0.0f,
+            1.0f,0.0f,0.0f,1.0f,
+
+    };
+
+
+    GLuint v_buffer_handle;
+    glGenBuffers(1, &v_buffer_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glGenVertexArrays(1, &vao_);
+    glBindVertexArray(vao_);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBindVertexArray(0);
+
+    GLuint v_buffer_handle2;
+    glGenBuffers(1, &v_buffer_handle2);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle2);
+    glBufferData(GL_ARRAY_BUFFER, vertices2.size() * sizeof(GLfloat), vertices2.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glGenVertexArrays(1, &vao_2);
+    glBindVertexArray(vao_2);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle2);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBindVertexArray(0);
+
+    GLuint v_buffer_handle3;
+    glGenBuffers(1, &v_buffer_handle3);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle3);
+    glBufferData(GL_ARRAY_BUFFER, vertices3.size() * sizeof(GLfloat), vertices3.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glGenVertexArrays(1, &vao_3);
+    glBindVertexArray(vao_3);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle3);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBindVertexArray(0);
+
+
+
+
+    glClearColor(0.81f, 0.81f, 0.8f, 1.0f);
+    int w, h;
+    std::tie(w, h) = frame_buffer_size();
+    glViewport(0, 0, w, h);
+
+    glEnable(GL_DEPTH_TEST);
+    glUseProgram(program);
+}
+
+void SimpleShapeApplication::frame() {
+    glBindVertexArray(vao_);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_SHORT,reinterpret_cast<GLvoid*>(0));
+    glBindVertexArray(0);
+
+    glBindVertexArray(vao_2);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_SHORT,reinterpret_cast<GLvoid*>(0));
+    glBindVertexArray(0);
+
+    glBindVertexArray(vao_3);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_SHORT,reinterpret_cast<GLvoid*>(0));
+    glBindVertexArray(0);
+}
