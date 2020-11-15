3b4abfce72bd181ed0e579b6c59605fa036d6e9b
izabela-lukaszek <73258446+izabela-lukaszek@users.noreply.github.com>
2020-11-15 23:11:12 +0100

    Update app.cpp

--- src/Exercises/Uniforms/app.cpp
+++ src/Exercises/Uniforms/app.cpp
@@ -24,7 +24,7 @@ void SimpleShapeApplication::init() {
     }
 
     std::vector<GLushort> indices = {
-            0,1,2,3,4,5,6,7,8,
+            0,1,2,3,4,5,3,5,6
     };
     GLuint idx_buffer_handle;
     glGenBuffers(1,&idx_buffer_handle);
@@ -55,38 +55,27 @@ void SimpleShapeApplication::init() {
     glBindBuffer(GL_UNIFORM_BUFFER, 0);
     glBindBufferBase(GL_UNIFORM_BUFFER, 0, ubo_handle);
 
+
     std::vector<GLfloat> vertices = {
-            -0.5f, -0.5f, 0.0f,
-            0.0f,0.0f,1.0f,1.0f,
-            -0.5f, 0.0f, 0.0f,
-            0.0f,0.0f,1.0f,1.0f,
-            0.0f, 0.0f, 0.0f,
-            0.0f,0.0f,1.0f,1.0f,
 
-    };
-
-
-    std::vector<GLfloat> vertices2 = {
-            0.0f, 0.0f, 0.0f,
-            0.0f,0.0f,1.0f,1.0f,
-            0.0f, -0.5f, 0.0f,
-            0.0f,0.0f,1.0f,1.0f,
-            -0.5f, -0.5f, 0.0f,
-            0.0f,0.0f,1.0f,1.0f,
-
-    };
-
-
-    std::vector<GLfloat> vertices3 = {
             -0.7f, 0.0f, 0.0f,
-            1.0f,0.0f,0.0f,1.0f,
+            1.0f,0.0f,0.0f,//kolor
             0.2f, 0.0f, 0.0f,
-            1.0f,0.0f,0.0f,1.0f,
+            1.0f,0.0f,0.0f,//kolor
             -0.25f, 0.6f, 0.0f,
-            1.0f,0.0f,0.0f,1.0f,
+            1.0f,0.0f,0.0f,//kolor
+
+            -0.5f, -0.5f, 0.0f,
+            0.0f,0.0f,1.0f,//kolor
+            -0.5f, 0.0f, 0.0f,
+            0.0f,0.0f,1.0f,//kolor
+            0.0f, 0.0f, 0.0f,
+            0.0f,0.0f,1.0f,//kolor
+
+            0.0f, -0.5f, 0.0f,
+            0.0f,0.0f,1.0f,//kolor
 
     };
-
 
     GLuint v_buffer_handle;
     glGenBuffers(1, &v_buffer_handle);
@@ -98,49 +87,12 @@ void SimpleShapeApplication::init() {
     glBindVertexArray(vao_);
     glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
     glBindVertexArray(0);
-
-    GLuint v_buffer_handle2;
-    glGenBuffers(1, &v_buffer_handle2);
-    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle2);
-    glBufferData(GL_ARRAY_BUFFER, vertices2.size() * sizeof(GLfloat), vertices2.data(), GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glGenVertexArrays(1, &vao_2);
-    glBindVertexArray(vao_2);
-    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle2);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
-    glBindVertexArray(0);
-
-    GLuint v_buffer_handle3;
-    glGenBuffers(1, &v_buffer_handle3);
-    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle3);
-    glBufferData(GL_ARRAY_BUFFER, vertices3.size() * sizeof(GLfloat), vertices3.data(), GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glGenVertexArrays(1, &vao_3);
-    glBindVertexArray(vao_3);
-    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle3);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
-    glBindVertexArray(0);
-
-
-
 
     glClearColor(0.81f, 0.81f, 0.8f, 1.0f);
     int w, h;
@@ -154,16 +106,7 @@ void SimpleShapeApplication::init() {
 void SimpleShapeApplication::frame() {
     glBindVertexArray(vao_);
     //glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_SHORT,reinterpret_cast<GLvoid*>(0));
-    glBindVertexArray(0);
-
-    glBindVertexArray(vao_2);
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_SHORT,reinterpret_cast<GLvoid*>(0));
-    glBindVertexArray(0);
-
-    glBindVertexArray(vao_3);
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_SHORT,reinterpret_cast<GLvoid*>(0));
+    glDrawElements(GL_TRIANGLES,9,GL_UNSIGNED_SHORT,reinterpret_cast<GLvoid*>(0));
     glBindVertexArray(0);
 }
+
