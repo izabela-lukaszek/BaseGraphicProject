30a570cc21bbabf731f686985aceabed56b4e4b2
izabela-lukaszek <73258446+izabela-lukaszek@users.noreply.github.com>
2021-01-05 14:29:21 +0100

    Update app.cpp
    Poprawione zadanie PVM

--- src/Exercises/PVM/app.cpp
+++ src/Exercises/PVM/app.cpp
@@ -3,16 +3,16 @@
 //
 
 #include "app.h"
-
 #include <iostream>
 #include <vector>
 #include <tuple>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/string_cast.hpp>
 #include <glm/glm.hpp>
-
-
 #include "Application/utils.h"
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 void SimpleShapeApplication::init() {
 
@@ -29,45 +29,6 @@ void SimpleShapeApplication::init() {
     std::vector<GLushort> indices = {
             0,1,2,3,4,5,3,5,6
     };
-    GLuint idx_buffer_handle;
-    glGenBuffers(1,&idx_buffer_handle);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(),GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    GLuint ubo_handle(0u);
-    glGenBuffers(1,&ubo_handle);
-
-    glBindBuffer(GL_UNIFORM_BUFFER, ubo_handle);
-    float light_intensity=0.9;
-    float light_color[3] = {1.0,0.0,0.0};
-
-    glBufferData(GL_UNIFORM_BUFFER, 8 * sizeof(float), nullptr, GL_STATIC_DRAW);
-
-    glBufferSubData(GL_UNIFORM_BUFFER,0,sizeof(float),&light_intensity);
-    glBufferSubData(GL_UNIFORM_BUFFER,4 * sizeof(float),3 * sizeof(float),light_color);
-
-    glBindBuffer(GL_UNIFORM_BUFFER, 0);
-    glBindBufferBase(GL_UNIFORM_BUFFER, 0, ubo_handle);
-
-    auto u_modifiers_index = glGetUniformBlockIndex(program, "Modifiers");
-    if (u_modifiers_index == GL_INVALID_INDEX){
-        std::cout << "Cannot find Modifiers uniform block in program" << std::endl;
-    }
-    else {
-        glUniformBlockBinding(program, u_modifiers_index, 0);
-    }
-
-    auto u_transformations_index = glGetUniformBlockIndex(program, "Transformations");
-    if (u_transformations_index == GL_INVALID_INDEX)
-    {
-        std::cout << "Cannot find Transformations uniform block in program" << std::endl;
-    }
-    else
-    {
-        glUniformBlockBinding(program, u_transformations_index, 1);
-    }
-
 
     std::vector<GLfloat> vertices = {
 
@@ -96,37 +57,78 @@ void SimpleShapeApplication::init() {
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+    GLuint idx_buffer_handle;
+    glGenBuffers(1,&idx_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(),GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
     glGenVertexArrays(1, &vao_);
     glBindVertexArray(vao_);
     glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+
+
+    auto u_modifiers_index = glGetUniformBlockIndex(program, "Modifiers");
+    if (u_modifiers_index == GL_INVALID_INDEX){
+        std::cout << "Cannot find Modifiers uniform block in program" << std::endl;
+    }
+    else {
+        glUniformBlockBinding(program, u_modifiers_index, 0);
+    }
+
+    GLuint ubo_handle[2];
+    glGenBuffers(2,ubo_handle);
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo_handle[0]);
+    glBufferData(GL_UNIFORM_BUFFER, 8 * sizeof(float), nullptr, GL_STATIC_DRAW);
+
+
+    float light_intensity=1.0;
+    float light_color[3] = {0.5,0.5,0.5};
+    glBufferSubData(GL_UNIFORM_BUFFER,0,sizeof(float),&light_intensity);
+    glBufferSubData(GL_UNIFORM_BUFFER,4 * sizeof(float),3 * sizeof(float),light_color);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    glBindBufferBase(GL_UNIFORM_BUFFER, 0, ubo_handle[0]);
+
+
+    auto u_transformations_index = glGetUniformBlockIndex(program, "Transformations");
+    if (u_transformations_index == GL_INVALID_INDEX)
+    {
+        std::cout << "Cannot find Transformations uniform block in program" << std::endl;
+    }
+    else
+    {
+        glUniformBlockBinding(program, u_transformations_index, 1);
+    }
+
+    glBindBuffer(GL_UNIFORM_BUFFER,ubo_handle[1]);
+    glBufferData(GL_UNIFORM_BUFFER,2 * sizeof(glm::mat4), nullptr,GL_STATIC_DRAW);
+
+    int w, h;
+    std::tie(w, h) = frame_buffer_size();
+    auto V = glm::lookAt(glm::vec3{0.1,1.0,0.5},glm::vec3{0.0,0.0,0.0},glm::vec3{0.0,1.0,0.0});
+    auto P = glm::perspective(glm::half_pi<float>(),(float)w/h,0.1f,100.0f);
+
+    glBufferSubData(GL_UNIFORM_BUFFER,0,sizeof(glm::mat4),&P[0]);
+    glBufferSubData(GL_UNIFORM_BUFFER,sizeof(glm::mat4),sizeof(glm::mat4),&V[0]);
+    glBindBuffer(GL_UNIFORM_BUFFER,0);
+    glBindBufferBase(GL_UNIFORM_BUFFER,1,ubo_handle[1]);
+
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
     glBindVertexArray(0);
 
     glClearColor(0.81f, 0.81f, 0.8f, 1.0f);
-    int w, h;
-    std::tie(w, h) = frame_buffer_size();
-    glm::mat4 M(1.0f);
-    auto V = glm::lookAt(glm::vec3{0.1,1.0,0.5},glm::vec3{0.0,0.0,0.0},glm::vec3{0.0,1.0,0.0});
-    auto P = glm::perspective(glm::half_pi<float>(),(float)w/h,0.1f,100.0f);
-    glBufferData(GL_UNIFORM_BUFFER,2*sizeof(glm::mat4), nullptr,GL_STATIC_DRAW);
-    glBufferSubData(GL_UNIFORM_BUFFER,0,sizeof(glm::mat4),&P[0]);
-    glBufferSubData(GL_UNIFORM_BUFFER,sizeof(glm::mat4),sizeof(glm::mat4),&V[0]);
-    glBindBuffer(GL_UNIFORM_BUFFER,0);
-    glBindBufferBase(GL_UNIFORM_BUFFER,1,ubo_handle);
     glViewport(0, 0, w, h);
-
-
     glEnable(GL_DEPTH_TEST);
     glUseProgram(program);
 }
 
 void SimpleShapeApplication::frame() {
     glBindVertexArray(vao_);
-    glDrawElements(GL_TRIANGLES,9,GL_UNSIGNED_SHORT,reinterpret_cast<GLvoid*>(0));
+    glDrawElements(GL_TRIANGLES,9,GL_UNSIGNED_SHORT, nullptr);
     glBindVertexArray(0);
 }
